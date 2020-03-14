b2937a1766aebdf6944aba76bb64e3b8c8970114
adpineiro <adpineiro@alumnos.uvigo.es>
2020-03-14 21:24:18 +0100

    Creados botones pantalla y funciones iniciales para pantalla

--- Brazo_robotico/src/main.cpp
+++ Brazo_robotico/src/main.cpp
@@ -9,20 +9,37 @@
 
 // inicializacion servos
 Adafruit_PWMServoDriver pwm=Adafruit_PWMServoDriver();
-int ang_giro=90;
-int ang_1 = 90;
-int ang_2 = 90;
-int giro_mano = 90;
-int ang_apert = 90;
 
 // inicializacion pantalla
 
-NexButton b0=NexButton(0, 1, "b0");
+//decalracion de variables de la pantalla
+//NexButton (pagina, componente, "nombre")
+NexButton b0 =NexButton(0, 1, "b0");
+NexButton b1 =NexButton(0, 2, "b1");
+NexButton b2 =NexButton(0, 3, "b2");
+NexButton b3 =NexButton(0, 4, "b3");
+NexButton b4 =NexButton(0, 5, "b4");
+NexButton b5 =NexButton(0, 6, "b5");
+NexButton b6 =NexButton(0, 7, "b6");
+NexButton b7 =NexButton(0, 8, "b7");
+NexButton b8 =NexButton(0, 9, "b8");
+NexButton b9 =NexButton(0, 10, "b9");
 
-NexTouch *nex_listen_list[]=
+char buffer[100]={0}; //buffer para leer los mensajes de la pantalla
+
+NexTouch *nex_listen_list[]= //Registra un objeto boton en la lista de eventos
 {
-  &b0,
-  NULL
+&b0,
+&b1,
+&b2,
+&b3,
+&b4,
+&b5,
+&b6,
+&b7,
+&b8,
+&b9,
+NULL
 };
 
 void setPosition(int angulo, int servonum){
@@ -32,33 +49,34 @@ void setPosition(int angulo, int servonum){
 
 }
 
-//Acciones a los botones de la pantalla
-void b0PopCallback(void *ptr){
-// boton1
-
-}
-
-void update_nextion(){
-  // actualizar los datos de la pantalla
-}
-
 void setup() {
+  //inicializacion pantalla
+  nexInit();
+  b0.attachPush(b0PushCallback, &b0);
+  b1.attachPush(b1PushCallback, &b1);
+  b2.attachPush(b2PushCallback, &b2);
+  b3.attachPush(b3PushCallback, &b3);
+  b4.attachPush(b4PushCallback, &b4);
+  b5.attachPush(b5PushCallback, &b5);
+  b6.attachPush(b6PushCallback, &b6);
+  b7.attachPush(b7PushCallback, &b7);
+  b8.attachPush(b8PushCallback, &b8);
+  b9.attachPush(b9PushCallback, &b9);
+
   //inicializacion servos
-  Serial.begin(9600);
-  Serial.println("Inicio del programa");
   pwm.begin();
   pwm.setPWMFreq(50);
-
-// inicializacion pantalla
-  nexInit();
-  b0.attachPop(b0PopCallback, &b0);
-
-
-  delay(10);
-
+  //Inicializacion OK
+  Serial.begin(9600);
+  Serial.println("Inicio del programa");
 }
 
 void loop() {
   // bucle lectura pantalla
   nexLoop(nex_listen_list);
+}
+
+//Respuesta al evento boton pulsado
+void b0PushCallback(void *ptr){
+
 }
